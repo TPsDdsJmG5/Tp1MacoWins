d89dfb4fe335b3eed7bc105a761f7039fb623f6e
GastonTS <gastonsantoalla@gmail.com>
2015-04-15 18:50:05 -0300

    Actualizacion readme.
    Desarrolle un poco los puntos que había puesto Gustavo.

--- tp1MacoWins/src/main/java/ar/edu/grupo5/jm/dss/macoWins/Armani.java
+++ tp1MacoWins/src/main/java/ar/edu/grupo5/jm/dss/macoWins/Armani.java
@@ -2,7 +2,6 @@ package ar.edu.grupo5.jm.dss.macoWins;
 
 public class Armani implements Marca {
 
-	@Override
 	public double factorMarca(double unPrecioOriginal) {
 		return 1.65;
 	}
--- tp1MacoWins/src/main/java/ar/edu/grupo5/jm/dss/macoWins/Sarkany.java
+++ tp1MacoWins/src/main/java/ar/edu/grupo5/jm/dss/macoWins/Sarkany.java
@@ -2,7 +2,6 @@ package ar.edu.grupo5.jm.dss.macoWins;
 
 public class Sarkany implements Marca {
 
-	@Override
 	public double factorMarca(double unPrecioOriginal) {
 		if (unPrecioOriginal > 500) 
 			return 1.35;
