080c2a58533b757e0c23443772cd3375a44f65c5
GastonTS <gastonsantoalla@gmail.com>
2015-04-27 16:10:16 -0300

    Divido y cambio el nombre del test de Negocio

--- tp1MacoWins/src/test/java/ar/edu/grupo5/jm/dss/macoWins/NegocioTest.java
+++ tp1MacoWins/src/test/java/ar/edu/grupo5/jm/dss/macoWins/NegocioTest.java
@@ -18,27 +18,45 @@ public class NegocioTest {
 
 	@Before
 	public void Setup(){
-		pantalonFeo = new Pantalon(200, new Nacional(), new Sarkany());
+		pantalonFeo = new Pantalon(200, new Internacional(), new Sarkany());
 		camisaNoTanFea = new Camisa(new Nacional(), new Sarkany());
-		sacoLoco = new Saco(15, new Nacional(), new Armani());
+		sacoLoco = new Saco(15, new Internacional(), new Armani());
 		zapatito = new Zapato(25, new Nacional(), new Sarkany());
-		sombrerito = new Sombrero(175, new Internacional(), new Armani());		
+		sombrerito = new Sombrero(175, new Nacional(), new Armani());		
 
 		negocio = new Negocio(100);
 		}
 	
 		
 	@Test
-	public void test() {
+	public void ventaCamisaYSacoDaResultadoCorrecto() {
 
-		negocio.realizarVenta(pantalonFeo, 2, LocalDate.parse("2015-04-02"));
 		negocio.realizarVenta(camisaNoTanFea, 3, LocalDate.parse("2015-04-02"));
 		negocio.realizarVenta(sacoLoco, 1, LocalDate.parse("2015-04-02"));
-		negocio.realizarVenta(zapatito, 1, LocalDate.parse("2015-04-02"));
+				
+		Double ganancia = negocio.gananciaEn(LocalDate.parse("2015-04-02"));
+		
+		assertEquals(2169.75,ganancia,0.01);
+	}
+	
+	@Test
+	public void ventaSombreroDaResultadoCorrecto() {
+
 		negocio.realizarVenta(sombrerito, 5, LocalDate.parse("2015-04-02"));
 		
 		Double ganancia = negocio.gananciaEn(LocalDate.parse("2015-04-02"));
 		
-		assertEquals((Double)8784.375,ganancia);
+		assertEquals(3506.25,ganancia,0.01);
+	}
+	
+	@Test
+	public void ventaPantalonYZapatoDaResultadoCorrecto() {
+
+		negocio.realizarVenta(pantalonFeo, 2, LocalDate.parse("2015-04-02"));
+		negocio.realizarVenta(zapatito, 1, LocalDate.parse("2015-04-02"));
+		
+		Double ganancia = negocio.gananciaEn(LocalDate.parse("2015-04-02"));
+		
+		assertEquals(2774.25,ganancia,0.01);
 	}
 }
